20b5774b273e3e232cd7f4d3042196f18a7eade7
erisch <erik.schlegel@gmail.com>
2017-08-28 17:30:39 -0500

    Adding heatmap by tileid service

--- src/schemas/TilesSchema.js
+++ src/schemas/TilesSchema.js
@@ -2,11 +2,8 @@ const graphql = require('graphql');
 
 module.exports = graphql.buildSchema(`
   type Query {
-    fetchTilesByBBox(site: String!, bbox: [Float]!, mainEdge: String!, filteredEdges: [String], timespan: String!, zoomLevel: Int, sourceFilter: [String], fromDate: String, toDate: String, originalSource: String): FeatureCollection,
-    fetchPlacesByBBox(site: String!, bbox: [Float]!, zoom: Int): PlaceCollection,
+    heatmapFeaturesByTile(fromDate: String!, toDate: String!, periodType: String!, pipelinekeys: [String]!, maintopic: String!, conjunctivetopics: [String], tiley: Int!, tilex: Int!, zoomLevel: Int!, externalsourceid: String!): FeatureCollection,
     fetchTilesByLocations(site: String!, locations: [[Float]]!, mainEdge: String, zoomLevel: Int, filteredEdges: [String], timespan: String!, sourceFilter: [String], fromDate: String, toDate: String): FeatureCollection,
-    fetchEdgesByLocations(site: String!, locations: [[Float]]!, zoomLevel: Int, mainEdge: String, filteredEdges: [String], timespan: String!, sourceFilter: [String], fromDate: String, toDate: String): EdgeCollection,
-    fetchEdgesByBBox(site: String!, bbox: [Float]!, zoomLevel: Int, mainEdge: String!, timespan: String!, sourceFilter: [String], fromDate: String, toDate: String, originalSource: String): EdgeCollection
   }
 
   enum TypeEnum {
@@ -14,41 +11,13 @@ module.exports = graphql.buildSchema(`
   }
 
   enum FeatureType {
-    Point,
-    MultiPoint
+    Point
   }
 
   type FeatureCollection {
     runTime: String,
     type: TypeEnum!,
-    bbox: [Float],
     features: [Feature]!
-  }
-
-  type PlaceCollection {
-    runTime: String,
-    type: TypeEnum!,
-    bbox: [Float],
-    features: [PlaceFeature]!
-  }
-
-  type PlaceFeature {
-    coordinates: [Float],
-    name: String,
-    name_ar: String,
-    name_ur: String,
-    name_de: String,
-    name_id: String,
-    id: String,
-    population: Float,
-    kind: String,
-    tileId: ID,
-    source: String
-  }
-
-  type EdgeCollection {
-    runTime: String,
-    edges: [Edge]!
   }
 
   type Feature {
@@ -57,23 +26,12 @@ module.exports = graphql.buildSchema(`
     properties: Tile!
   }
 
-  enum EdgeType {
-      Term,
-      Location
-  }
-
-  type Edge {
-    type: EdgeType,
-    name: String,
-    mentionCount: Int
-  }
-
   type Tile {
-    mentionCount: Int,
-    location: String,
-    population: Float,
-    neg_sentiment: Float,
-    pos_sentiment: Float,
-    tileId: ID
+    mentions: Int
+    date: String
+    avgsentiment: Float
+    tilex: Int
+    tiley: Int
+    tilez: Int
   }
 `);
