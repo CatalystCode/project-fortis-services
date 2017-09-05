9880ac125dfafe97274466eea3fb33c23fd5a829
erisch <erik.schlegel@gmail.com>
2017-09-05 07:20:45 -0500

    Refactored services to integrate reoptimized cassandra tables

--- src/resolvers-cassandra/Edges/queries.js
+++ src/resolvers-cassandra/Edges/queries.js
@@ -86,7 +86,11 @@ function timeSeries(args, res) { // eslint-disable-line no-unused-vars
     const conjunctivetopics = args.maintopics.length > 1 ? [] : args.conjunctivetopics;
 
     const MaxConjunctiveTopicsAllowed = 2;
+<<<<<<< HEAD
     const dateFormat = 'YYYY-MM-DD HH:mm';
+=======
+    const dateFormat = "YYYY-MM-DD HH:mm"
+>>>>>>> Refactored services to integrate reoptimized cassandra tables
 
     const query = `
     SELECT conjunctiontopic1, conjunctiontopic2, conjunctiontopic3, perioddate, mentioncount, avgsentimentnumerator, tileid
@@ -233,11 +237,15 @@ function topSources(args, res) { // eslint-disable-line no-unused-vars
         })).slice(0, responseSize);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Refactored services to integrate reoptimized cassandra tables
         resolve({
           edges
         });
       })
       .catch(reject);
+<<<<<<< HEAD
 =======
       resolve({
         edges
@@ -245,6 +253,8 @@ function topSources(args, res) { // eslint-disable-line no-unused-vars
     })
     .catch(reject);
 >>>>>>> Reorganizing aggregation logic
+=======
+>>>>>>> Refactored services to integrate reoptimized cassandra tables
   });
 }
 
