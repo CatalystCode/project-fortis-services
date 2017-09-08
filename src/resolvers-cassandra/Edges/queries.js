9aa385ea461067f9bb8477938a9628ce3ef81b59
erisch <erik.schlegel@gmail.com>
2017-09-08 07:50:29 -0500

    Adding tile id list service for heatmap

--- src/resolvers-cassandra/Edges/queries.js
+++ src/resolvers-cassandra/Edges/queries.js
@@ -5,12 +5,16 @@ const moment = require('moment');
 const Long = require('cassandra-driver').types.Long;
 const cassandraConnector = require('../../clients/cassandra/CassandraConnector');
 const featureServiceClient = require('../../clients/locations/FeatureServiceClient');
-const { tilesForBbox, withRunTime, asCsvExporter, toConjunctionTopics, fromTopicListToConjunctionTopics } = require('../shared');
+const { tilesForBbox, withRunTime, toConjunctionTopics, fromTopicListToConjunctionTopics } = require('../shared');
 const { makeSet, makeMap, aggregateBy } = require('../../utils/collections');
 const { trackEvent } = require('../../clients/appinsights/AppInsightsClient');
 
 const MaxFetchedRows = 10000;
 
+/**
+ * @param {{limit: Int!, fromDate: String!, periodType: String!, toDate: String!, externalsourceid: String!, pipelinekeys: [String]!, bbox: [Float]}} args
+ * @returns {Promise.<{runTime: string, edges: Array<{name: string, mentions: number, placeid: string, avgsentiment: float}>}>}
+ */
 function popularLocations(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
     const fetchSize = 400;
@@ -76,25 +80,20 @@ function popularLocations(args, res) { // eslint-disable-line no-unused-vars
         } else {
           resolve({ edges: [] });
         }
-      })
-      .catch(reject);
+      });
   });
 }
 
+/**
+ * @param {{fromDate: String!, periodType: String!, toDate: String!, pipelinekeys: [String]!, maintopics: [String]!, conjunctivetopics: [String], bbox: [Float], zoomLevel: Int, externalsourceid: String!}} args
+ * @returns {Promise.<{labels: Array<{name: string, mentions: number}>, graphData: Array<{date: string, edges: string[], mentions: number[]}>}>}
+ */
 function timeSeries(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
     const conjunctivetopics = args.maintopics.length > 1 ? [] : args.conjunctivetopics;
 
     const MaxConjunctiveTopicsAllowed = 2;
-<<<<<<< HEAD
-<<<<<<< HEAD
     const dateFormat = 'YYYY-MM-DD HH:mm';
-=======
-    const dateFormat = "YYYY-MM-DD HH:mm"
->>>>>>> Refactored services to integrate reoptimized cassandra tables
-=======
-    const dateFormat = 'YYYY-MM-DD HH:mm';
->>>>>>> Fixing lint errors
 
     const query = `
     SELECT conjunctiontopic1, conjunctiontopic2, conjunctiontopic3, perioddate, mentioncount, avgsentimentnumerator, tileid
@@ -141,22 +140,26 @@ function timeSeries(args, res) { // eslint-disable-line no-unused-vars
   });
 }
 
+/**
+ * @param {{bbox: string}} args
+ * @returns {Promise.<{runTime: string, edges: Array<{name: string, coordinates: number[]}>}>}
+ */
 function locations(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
     const { bbox } = args;
 
     featureServiceClient.fetchByBbox({ north: bbox[0], west: bbox[1], south: bbox[2], east: bbox[3] }, 'bbox')
-      .then(locations => {
-        const places = locations.map(location => ({ name: location.name, placeid: location.id, layer: location.layer, bbox: location.bbox }));
-
-        resolve({
-          places
-        });
-      })
+      .then(locations =>
+        resolve(locations.map(location => ({ name: location.name, placeid: location.id, layer: location.layer, bbox: location.bbox })))
+      )
       .catch(reject);
   });
 }
 
+/**
+ * @param {{limit: Int!, fromDate: String!, periodType: String!, toDate: String!, externalsourceid: String!, pipelinekeys: [String]!, bbox: [Float], zoomLevel: Int}} args
+ * @returns {Promise.<{edges: Array<{name: string, mentions: number, avgsentiment: float}>}>}
+ */
 function topTerms(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
     const fetchSize = 400;
@@ -186,21 +189,24 @@ function topTerms(args, res) { // eslint-disable-line no-unused-vars
     ];
 
     return cassandraConnector.executeQuery(query, params, { fetchSize })
-      .then(rows => {
-        const edges = aggregateBy(rows, row => `${row.conjunctiontopic1}`, row => ({
-          name: row.conjunctiontopic1,
-          mentions: Long.ZERO,
-          avgsentimentnumerator: Long.ZERO
-        })).slice(0, responseSize);
-
+      .then(rows =>
         resolve({
-          edges
-        });
-      })
+          edges: aggregateBy(rows, row => `${row.conjunctiontopic1}`, row => ({
+            name: row.conjunctiontopic1,
+            mentions: Long.ZERO,
+            avgsentimentnumerator: Long.ZERO
+          }))
+            .slice(0, responseSize)
+        })
+      )
       .catch(reject);
   });
 }
 
+/**
+ * @param {{limit: Int!, fromDate: String!, periodType: String!, toDate: String!, pipelinekeys: [String]!, conjunctivetopics: [String]!, bbox: [Float], zoomLevel: Int}} args
+ * @returns {Promise.<{sources: Array<{Name: string, Count: number, Source: string}>}>}
+ */
 function topSources(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
     const fetchSize = 400;
@@ -238,30 +244,21 @@ function topSources(args, res) { // eslint-disable-line no-unused-vars
           name: row.externalsourceid,
           mentions: Long.ZERO,
           avgsentimentnumerator: Long.ZERO
-        })).slice(0, responseSize);
+        }))
+          .slice(0, responseSize);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Refactored services to integrate reoptimized cassandra tables
         resolve({
           edges
         });
       })
       .catch(reject);
-<<<<<<< HEAD
-=======
-      resolve({
-        edges
-      });
-    })
-    .catch(reject);
->>>>>>> Reorganizing aggregation logic
-=======
->>>>>>> Refactored services to integrate reoptimized cassandra tables
   });
 }
 
+/**
+ * @param {{fromDate: String!, periodType: String!, toDate: String!, externalsourceid: String!, pipelinekeys: [String]!, maintopic: String!, bbox: [Float], zoomLevel: Int!}} args
+ * @returns {Promise.<{sources: Array<{Name: string, Count: number, Source: string}>}>}
+ */
 function conjunctiveTopics(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
     const fetchSize = 400;
@@ -292,8 +289,9 @@ function conjunctiveTopics(args, res) { // eslint-disable-line no-unused-vars
 
     return cassandraConnector.executeQuery(query, params, { fetchSize })
       .then(rows => {
+        const filteredRows = rows.filter(topic=>topic.conjunctivetopic.toLowerCase() !== args.maintopic.toLowerCase());
         //todo: need to add sentiment field to the conjunctivetopics table
-        const edges = aggregateBy(rows, row => `${row.conjunctivetopic}`, row => ({
+        const edges = aggregateBy(filteredRows, row => `${row.conjunctivetopic}`, row => ({
           conjunctionterm: row.conjunctivetopic,
           name: row.topic,
           mentions: Long.ZERO
@@ -313,12 +311,5 @@ module.exports = {
   topTerms: trackEvent(topTerms, 'topTerms'),
   geofenceplaces: trackEvent(withRunTime(locations), 'locations'),
   conjunctiveTopics: trackEvent(conjunctiveTopics, 'conjunctiveTopics'),
-  topSources: trackEvent(topSources, 'topSources'),
-
-  csv_popularLocations: trackEvent(asCsvExporter(popularLocations, 'edges'), 'csv_popularLocations'),
-  csv_timeSeries: trackEvent(asCsvExporter(timeSeries, 'graphData'), 'csv_timeSeries'),
-  csv_topTerms: trackEvent(asCsvExporter(topTerms, 'edges'), 'csv_topTerms'),
-  csv_geofenceplaces: trackEvent(asCsvExporter(locations, 'places'), 'csv_locations'),
-  csv_conjunctiveTopics: trackEvent(asCsvExporter(conjunctiveTopics, 'edges'), 'csv_conjunctiveTopics'),
-  csv_topSources: trackEvent(asCsvExporter(topSources, 'edges'), 'csv_topSources')
+  topSources: trackEvent(topSources, 'topSources')
 };
